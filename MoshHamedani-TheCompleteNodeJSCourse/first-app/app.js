0c0edc2dfed8911af798e0a65f665bdd22961241
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-05-10 11:59:10 +0100

    Node Module System -> Creating a Module

--- MoshHamedani-TheCompleteNodeJSCourse/first-app/app.js
+++ MoshHamedani-TheCompleteNodeJSCourse/first-app/app.js
@@ -1,12 +1,1 @@
-console.log() // global
-
-setTimeout()
-clearTimeout()
-
-setInterval()
-clearInterval()
-
-var message = ''
-
-global.console.log()
-console.log(global.message)
+console.log(module)
---
+++ MoshHamedani-TheCompleteNodeJSCourse/first-app/logger.js
@@ -0,0 +1,8 @@
+const url = 'http://mylogger.id/log'
+
+function log(message) {
+  // Send an HTTP request
+  console.log(message)
+}
+
+module.exports.log = log
