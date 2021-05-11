95aa8228c5da4216dcf3939cd79258ec831c6603
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-05-11 16:42:04 +0100

    Building RESTful APIs Using Express -> Handling HTTP PUT Requests

--- MoshHamedani-TheCompleteNodeJSCourse/express-demo/index.js
+++ MoshHamedani-TheCompleteNodeJSCourse/express-demo/index.js
@@ -19,16 +19,9 @@ app.get('/api/courses', (req, res) => {
 })
 
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  }
-
-  const result = Joi.validate(req.body, schema)
-  console.log(result)
-
-  if (result.error) {
-    // 400 Bad Request
-    res.status(400).send(result.error.details[0].message)
+  const { error } = validateCourse(req.body)
+  if (error) {
+    res.status(400).send(error.details[0].message)
     return
   }
 
@@ -37,6 +30,21 @@ app.post('/api/courses', (req, res) => {
     name: req.body.name,
   }
   courses.push(course)
+  res.send(course)
+})
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id))
+  if (!course)
+    res.status(404).send('The course with the given ID was not found.')
+
+  const { error } = validateCourse(req.body)
+  if (error) {
+    res.status(400).send(error.details[0].message)
+    return
+  }
+
+  course.name = req.body.name
   res.send(course)
 })
 
@@ -50,3 +58,11 @@ app.get('/api/courses/:id', (req, res) => {
 // PORT
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  }
+
+  return Joi.validate(course, schema)
+}
