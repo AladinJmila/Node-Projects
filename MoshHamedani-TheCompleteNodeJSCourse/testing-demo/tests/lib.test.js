0545a27bf41c07a6be39ede8c2b393e586d3f594
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-05-24 14:45:37 +0100

    Unit Testing -> Texting Exceptions

--- MoshHamedani-TheCompleteNodeJSCourse/testing-demo/tests/lib.test.js
+++ MoshHamedani-TheCompleteNodeJSCourse/testing-demo/tests/lib.test.js
@@ -24,3 +24,61 @@ describe('greet', () => {
     expect(result).toContain('Mosh')
   })
 })
+
+describe('getCurrencies', () => {
+  it('should return supported currencies', () => {
+    const result = lib.getCurrencies()
+
+    // Too general
+    expect(result).toBeDefined()
+    expect(result).not.toBeNull()
+
+    // Too specific
+    // expect(result[0].toBe('USD'))
+    // expect(result[1].toBe('AUD'))
+    // expect(result[2].toBe('EUR'))
+    // expect(result.length).toBe(3)
+
+    // Proper way
+    expect(result).toContain('USD')
+    expect(result).toContain('AUD')
+    expect(result).toContain('EUR')
+
+    // Ideal way
+    expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']))
+  })
+})
+
+describe('getProduct', () => {
+  it('should return the product with the given id', () => {
+    const result = lib.getProduct(1)
+    // expect(result).toEqual({ id: 1, price: 10 })
+
+    expect(result).toMatchObject({ id: 1, price: 10 })
+
+    expect(result).toHaveProperty('id', 1)
+  })
+})
+
+describe('registerUser', () => {
+  it('should throw if username is falsy', () => {
+    // Null
+    // undefined
+    // NaN
+    // ''
+    // 0
+    // false
+    const args = [null, undefined, NaN, '', 0, false]
+    args.forEach(a => {
+      expect(() => {
+        lib.registerUser(a)
+      }).toThrow()
+    })
+  })
+
+  it('should return a user object if valid username is passed', () => {
+    const result = lib.registerUser('mosh')
+    expect(result).toMatchObject({ username: 'mosh' })
+    expect(result.id).toBeGreaterThan(0)
+  })
+})
