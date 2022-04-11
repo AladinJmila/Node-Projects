b38bc31a9172eeb862a98673f87f39f5e6182f38
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-04-11 16:51:02 +0100

    Vidjot initial commit: initialized app and added express and handlebars

---
+++ bt-vidjot/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+const app = express();
+
+// Handlebars middleware
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome 2';
+  res.render('index', { title });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+const port = 4500;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
