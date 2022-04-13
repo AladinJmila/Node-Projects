8276dd447298a0c9f144d87bf68c62aa8a7aadc2
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-04-13 16:26:33 +0100

    Successfully connected to Google Oauth api

--- bt-story-books/app.js
+++ bt-story-books/app.js
@@ -1,9 +1,14 @@
-const express = require('express');
 const mongoose = require('mongoose');
-
+const express = require('express');
 const app = express();
+const passport = require('passport');
+require('./config/passport')(passport);
+
+const auth = require('./routes/auth');
 
 const port = process.env.PORT || 4500;
+
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
