a06d61c4ad1887d2025a8b70e62ec4823ccf93eb
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-04-14 15:24:50 +0100

    Story model

---
+++ bt-story-books/models/story.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const Story = mongoose.model(
+  'Storie',
+  new Schema({
+    creatorId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      defualt: 'public',
+    },
+    allowComments: {
+      type: Boolean,
+      default: true,
+    },
+    comments: [
+      {
+        commentBody: {
+          type: String,
+          required: true,
+        },
+        commentDate: {
+          type: Date,
+          default: Date.now,
+        },
+        commentUser: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        },
+      },
+    ],
+  })
+);
+
+exports.Story = Story;
--- bt-story-books/models/user.js
+++ bt-story-books/models/user.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const User = mongoose.model('User', {
-  googleID: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  firstName: String,
-  lastName: String,
-  image: String,
-});
+const User = mongoose.model(
+  'User',
+  new Schema({
+    googleID: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    firstName: String,
+    lastName: String,
+    image: String,
+  })
+);
 
 exports.User = User;
