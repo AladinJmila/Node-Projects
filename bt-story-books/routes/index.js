3b2c23c9f11103362877206a5cdb8e521c6caecc
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-04-15 10:20:57 +0100

    Populated dashboard

--- bt-story-books/routes/index.js
+++ bt-story-books/routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { auth, guest } = require('../services/auth');
+const { Story } = require('../models/story');
 
 router.get('/', guest, (req, res) => {
   res.render('index/welcome');
 });
 
-router.get('/dashboard', auth, (req, res) => {
-  res.render('index/dashboard');
+router.get('/dashboard', auth, async (req, res) => {
+  const stories = await Story.find({ user: req.user.id }).sort('-_id').lean();
+  res.render('index/dashboard', { stories });
 });
 
 router.get('/about', (req, res) => {
